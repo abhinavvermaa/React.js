1a29f947eb7f00d35163d3a3607c04bee5ad8ceb
abhinavvermaa <abhinavvermax@gmail.com>
2024-07-12 22:23:10 +0530

    calculator changes done

--- projects/4-calculator-version-1/calculator/src/App.jsx
+++ projects/4-calculator-version-1/calculator/src/App.jsx
@@ -1,12 +1,14 @@
-
+import styles from "./App.module.css";
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-    
-    </>
+  return  (<div className={styles.calculator}>
+    <input type="text" className={styles.display} />
+    <div id="buttons-container"></div>
+    <button>C</button>
+  </div>
+
   )
 }
 
