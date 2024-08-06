2759856706e62044d8a68689ef88e52aeb3b73be
abhinavvermaa <157112712+abhinavvermaa@users.noreply.github.com>
2024-08-06 12:14:24 +0530

    update app.jsx

--- 17 Bootstrap/learning-bootstrap/src/App.jsx
+++ 17 Bootstrap/learning-bootstrap/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
 
-  return <div>
+  return <>
             <button type="button" class="btn btn-primary">Primary</button>
         <button type="button" class="btn btn-secondary">Secondary</button>
         <button type="button" class="btn btn-success">Success</button>
@@ -14,7 +14,7 @@ function App() {
         <button type="button" class="btn btn-dark">Dark</button>
 
         <button type="button" class="btn btn-link">Link</button>
-  </div>
+  </>
 }
 
 export default App
