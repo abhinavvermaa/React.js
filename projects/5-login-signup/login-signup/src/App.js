7794129ae52d94645b334926fdd4a3a4082fa6b4
abhinavvermaa <abhinavvermax@gmail.com>
2024-08-09 03:39:52 +0530

    completed the Login Signup page project

--- projects/5-login-signup/login-signup/src/App.js
+++ projects/5-login-signup/login-signup/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginSignup from './components/loginSignup/LoginSignup';
 
 function App() {
   return (
     <div >
-      
+      <LoginSignup />
     </div>
   );
 }
