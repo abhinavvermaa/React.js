413364d0e3a05f77d2bd22611f05426abae4cb8e
abhinavvermaa <abhinavvermax@gmail.com>
2024-12-30 23:46:57 +0530

    started redux

---
+++ React-pro/11reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+ 
+
+const initialState = {
+  todos: [{id: 1, text:"Hello world"}]
+}
+
+export const todoSlice = createSlice({
+  name:'todo',
+  initialState,
+
+})
