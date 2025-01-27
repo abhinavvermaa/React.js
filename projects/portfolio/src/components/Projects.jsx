fba0b5e7eb518a2c74146d0a4e7fed9049bdf211
abhinavvermaa <abhinavvermax@gmail.com>
2025-01-27 21:53:19 +0530

    Portfolio website completed .

---
+++ projects/portfolio/src/components/Projects.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className=" flex-col justify-center border-transparent rounded-3xl md:mx-40 mx-4 p-4  m-10" id="projects">
+      <div>
+        <h1 className="text-4xl font-bold text-gray-100 m-5">PROJECTS</h1>
+      </div>
+      <div className="flex-wrap md:flex">
+        <div className=" flex-wrap items-center m-5 max-w-70  p-4 bg-slate-900/60  border-transparent rounded-3xl">
+          <div>
+            <img src="./assets/projects/project.png" alt="" />
+          </div>
+          <div className="">
+            <h1 className="text-2xl font-semibold ">Web EMS </h1>
+            <p className="text-xl">
+              This is a project made to learn the latest languages by building
+              an app.
+            </p>
+          </div>
+          
+            <div className="flex flex-row flex-wrap"><p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              React
+            </p>
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              express
+            </p>
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              Node
+            </p>
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              Node
+            </p></div>
+          
+          <div className="flex justify-center">
+            <a
+              className="m-4 py-1 px-10 bg-gray-300  border rounded-2xl text-black "
+              href="https://github.com/abhinavvermaa"
+              target="_blank"
+            >
+              source code
+            </a>
+          </div>
+        </div>
+        <div className="flex-wrap items-center m-5 max-w-70  p-4 bg-slate-900/60  border-transparent rounded-3xl">
+          <div>
+            <img src="./assets/projects/project.png" alt="" />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold ">Web EMS </h1>
+            <p className="text-xl">
+              This is a project made to learn the latest languages by building
+              an app.
+            </p>
+          </div>
+          <div className="flex flex-row flex-wrap">
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              React
+            </p>
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              express
+            </p>
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              Node
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <a
+              className="m-4 py-1 px-10 bg-gray-300  border rounded-2xl text-black "
+              href="https://github.com/abhinavvermaa"
+              target="_blank"
+            >
+              source code
+            </a>
+          </div>
+        </div>
+        <div className="flex-wrap items-center m-5 max-w-70  p-4 bg-slate-900/60  border-transparent rounded-3xl">
+          <div>
+            <img src="./assets/projects/project.png" alt="" />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold ">Web EMS </h1>
+            <p className="text-xl">
+              This is a project made to learn the latest languages by building
+              an app.
+            </p>
+          </div>
+          <div className="flex flex-row flex-wrap">
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              React
+            </p>
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              express
+            </p>
+            <p className="m-3 px-3 border-transparent rounded-2xl bg-blue-400 text-black">
+              Node
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <a
+              className="m-4 py-1 px-10 bg-gray-300  border rounded-2xl text-black "
+              href="https://github.com/abhinavvermaa"
+              target="_blank"
+            >
+              source code
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
